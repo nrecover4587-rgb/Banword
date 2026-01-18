50c1875c1fef273d82e034e281a015f27a50d4b5
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-18 19:34:31 +0530

    Update __main__.py

--- Banword/__main__.py
+++ Banword/__main__.py
@@ -12,8 +12,8 @@ async def roy_bot():
         importlib.import_module("Banword.modules." + all_module)
     print("• @aaradhyavideBot B𝗈𝗍 Started Successfully.")
     await idle()
-    print("• Don't edit baby, otherwise you get an error: @networkxlog")
-    await Banword.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @aashikteam**")
+    print("• Don't edit baby, otherwise you get an error: @codexempire")
+    await Banword.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @codexempire**")
 
 if __name__ == "__main__":
     loop.run_until_complete(roy_bot())
